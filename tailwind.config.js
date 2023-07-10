f637241bb44161ef13889105869082990f5aed9d
The-Gae-Bolg <juan_america2014@hotmail.es>
2023-07-10 17:53:46 -0600

    Team page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'sans-serif'],
+      'PTSerif': ['PT Serif', 'sans-serif'],
+      'Poppins': ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
